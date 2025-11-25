0fa878953117c97d7348ea598b89efa604ea9902
Qiong <102568256+QiongLouLi@users.noreply.github.com>
2025-11-25 21:18:25 +0800

    Update TSCNet.py

--- models/TSCNet.py
+++ models/TSCNet.py
@@ -118,44 +118,49 @@ class TSCoupler(nn.Module):
         I = I.permute(0, 2, 1)
         return I
 
+# decompose
 def _ensure_odd(k: int) -> int:
     return k if (k % 2 == 1) else (k + 1)
-# decompose
-class decomp_Gaussian(nn.Module):
+
+class decomp_Gaussian01(nn.Module):
     def __init__(self, k: int = 25, sigma_init: float = 3.0):
         super().__init__()
         self.k = _ensure_odd(k)
         self.log_sigma = None
         self._built_C = None
+        self.sigma_init = sigma_init
 
-    def _build_if_needed(self, C: int, device, dtype, sigma_init: float = 3.):
+    def _build_if_needed(self, C: int, device, dtype):
+
         if (self.log_sigma is None) or (self._built_C != C):
             self.log_sigma = nn.Parameter(
-                torch.full((C,), math.log(sigma_init), device=device, dtype=dtype)
+                torch.full((C,), math.log(self.sigma_init), device=device, dtype=dtype)
             )
             self._built_C = C
 
-    def _gaussian_kernel(self, C: int, device, dtype):
-        half = self.k // 2
-        idx = torch.arange(-half, half + 1, device=device, dtype=dtype)
-        sigma = torch.exp(self.log_sigma).view(C, 1, 1) + 1e-6
-        g = torch.exp(-(idx.view(1, 1, self.k) ** 2) / (2 * sigma * sigma))
+    def _gaussian_kernel(self, C: int, device, dtype, k_eff: int):
+        half = k_eff // 2
+        idx = torch.arange(-half, half + 1, device=device, dtype=dtype)  # [k_eff]
+        sigma = torch.exp(self.log_sigma).view(C, 1, 1) + 1e-6           # [C,1,1]
+        g = torch.exp(-(idx.view(1, 1, k_eff) ** 2) / (2 * sigma * sigma))  # [C,1,k_eff]
         g = g / (g.sum(dim=-1, keepdim=True) + 1e-12)
-        return g  # [C,1,k]
+        return g  # [C,1,k_eff]
 
-    def forward(self, x):  # x: [B,T,C]
-        B, T, C = x.shape
-        x_ch = x.permute(0, 2, 1)
-        self._build_if_needed(C, x.device, x.dtype, sigma_init=3.0)
-        w = self._gaussian_kernel(C, x.device, x.dtype)
+    def forward(self, x: torch.Tensor):
 
-        pad_left = self.k // 2
-        pad_right = self.k - 1 - pad_left
-        x_pad = F.pad(x_ch, (pad_left, pad_right), mode='reflect')
+        B, C, T = x.shape
+        device, dtype = x.device, x.dtype
+        self._build_if_needed(C, device, dtype)
+        k_eff = min(self.k, 2 * T - 1)
+        if k_eff % 2 == 0:
+            k_eff -= 1
+        half = k_eff // 2
 
+        w = self._gaussian_kernel(C, device, dtype, k_eff=k_eff)
+        x_pad = F.pad(x, (half, half), mode='reflect')
         trend = F.conv1d(x_pad, w, stride=1, padding=0, groups=C)
-        seasonal = x_ch - trend
-        return seasonal.permute(0, 2, 1), trend.permute(0, 2, 1)
+        seasonal = x - trend
+        return seasonal, trend
 
 # DCM module
 class DCM(nn.Module):
